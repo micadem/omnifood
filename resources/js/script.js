eaa753d78f28d47cf635787ccbfa54bfe4d13fe3
micadem <67447550+micadem@users.noreply.github.com>
2020-11-28 00:57:42 +0100

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,6 +60,29 @@ $('a[href*="#"]')
     }
   });
     
-    /*Animation on Scroll*/
+    /*Animations on Scroll*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated animate__fadeIn'); 
+    }, {
+        offset: '50%'
+    });
     
+     $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated animate__fadeInUp'); 
+    }, {
+        offset: '50%'
+    });
+    
+     $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated animate__fadeIn'); 
+    }, {
+        offset: '50%'
+    });
+    
+     $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated animate__pulse'); 
+    }, {
+        offset: '50%'
+    });
+ 
 });
